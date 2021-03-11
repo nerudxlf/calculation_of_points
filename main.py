c260bb1a9f9684fcdc5951958191c126fbe38135
nerudxlf <nerudxlf@gmail.com>
2021-03-11 16:21:11 +0600

    fix bugs

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import math
 import re
 
 import pandas as pd
 
 
 def filter_wos(data: object) -> object:
-    data = data.filter([ "Article Title", "N"])
-    data.rename(columns={"Article Title": "Title"}, inplace=True)
+    data = data.filter(["Article Title", "N"])
+    data.rename(columns={"Article Title": "Title", "N": "N_wos"}, inplace=True)
     data_list = data["Title"].to_list()
-    result_list = []
+    result_list: list = []
     for elem in data_list:
         if isinstance(elem, float):
             result_list.append("not")
@@ -22,7 +23,7 @@ def filter_wos(data: object) -> object:
 def filter_scopus(data: object) -> object:
     data = data.filter(["Title", "N"])
     data_list = data["Title"].to_list()
-    result_list = []
+    result_list: list = []
     for elem in data_list:
         if isinstance(elem, float):
             result_list.append("not")
@@ -34,23 +35,26 @@ def filter_scopus(data: object) -> object:
     return data
 
 
-def count_total(data: object, flag: bool, iw1: float, iw2: float, total: float) -> float:
-    n_list = data["N"].to_list()
+def count_total(data: object, flag: bool, iw1: float, total: float, n: str) -> float:
+    n_list: list = data[n].to_list()
     for i in n_list:
+        if math.isnan(float(i)):
+            continue
         if flag and float(i) < 1:
-            total += float(i) * iw1
+            total += float(i) * iw1 * 1.2
         else:
-            total += float(i) * iw2
+            total += float(i) * iw1
     return total
 
 
 def main():
-    iw1_1, iw1_2 = 138, 138
-    iw2_1, iw2_2 = 51.1, 51.1
-    iw3_1, iw3_2 = 18.9, 18.9
-    iw4_1, iw4_2 = 7, 7
-    iw_other_1, iw_other_2 = 7, 7
-
+    iw1_1: float = 138
+    iw2_1: float = 51.1
+    iw3_1: float = 18.9
+    iw4_1: float = 7
+    iw_other_1: float = 7
+    n_wos: str = "N_wos"
+    n: str = "N"
     flag: bool = False
     total: float = 0
 
@@ -65,14 +69,19 @@ def main():
     w4_data = filter_wos(pd.read_excel("w4.xlsx"))
     w_none_data = filter_wos(pd.read_excel("w_none.xlsx"))
 
-    total += count_total(w1_data, flag, iw1_1, iw1_2, total)
-    total += count_total(w2_data, flag, iw2_1, iw2_2, total)
-    total += count_total(w3_data, flag, iw3_1, iw3_2, total)
-    total += count_total(w4_data, flag, iw4_1, iw4_2, total)
-    total += count_total(w_none_data, flag, iw_other_1, iw_other_2, total)
-    print(total)
+    total = count_total(w1_data, flag, iw1_1, total, n_wos)
+    total = count_total(w2_data, flag, iw2_1, total, n_wos)
+    total = count_total(w3_data, flag, iw3_1, total, n_wos)
+    total = count_total(w4_data, flag, iw4_1, total, n_wos)
+    total = count_total(w_none_data, flag, iw_other_1, total, n_wos)
     all_data = pd.concat(
         [s1_data, s2_data, s3_data, s4_data, s_none_data, w1_data, w2_data, w3_data, w4_data, w_none_data])
-    all_data = all_data.drop_duplicates(keep=False)
-    all_data["total"] = total
-    all_data.to_excel("all_data.xlsx", index=False)
+    df_data = all_data.filter(["KEY"])
+    print(len(df_data.index))
+    df_data = df_data.drop_duplicates(keep=False)
+    print(len(df_data.index))
+    result_data = pd.merge(left=df_data, right=all_data, left_on="KEY", right_on="KEY")
+    print(len(result_data.index), len(all_data.index))
+    total = count_total(result_data, flag, iw_other_1, total, n)
+    print(total)
+    result_data.to_excel("all_data.xlsx", index=False)
